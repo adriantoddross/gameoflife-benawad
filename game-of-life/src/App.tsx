c775d18d243dd58460f2ab5f7ea0ad04e22d7b3c
Adrian <adriantoddross@gmail.com>
2020-03-06 18:40:55 -0500

    render cells

--- game-of-life/src/App.tsx
+++ game-of-life/src/App.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+
+const numRows = 50;
+const numCols = 50;
 
 const App: React.FC = () => {
-  return <div>Hello World!</div>;
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  });
+  console.log(grid);
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 20px)`
+      }}
+    >
+      {grid.map((rows, i) =>
+        rows.map((col, k) => (
+          <div
+          key={`${i}-${k}`}
+            style={{
+              width: "20px",
+              height: "20px",
+              backgroundColor: grid[i][k] ? "pink" : undefined,
+              border: "1px solid black"
+            }}
+          ></div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default App;

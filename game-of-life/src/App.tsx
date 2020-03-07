6b244afa3328737bb955cb012ce40a983d95e52c
Adrian <adriantoddross@gmail.com>
2020-03-06 21:45:16 -0500

    test simulation

--- game-of-life/src/App.tsx
+++ game-of-life/src/App.tsx
@@ -10,8 +10,8 @@ const operations = [
   [-1, 1],
   [1, 1],
   [-1, -1],
-  [1,0],
-  [-1,0],
+  [1, 0],
+  [-1, 0]
 ];
 
 const App: React.FC = () => {
@@ -32,30 +32,45 @@ const App: React.FC = () => {
       return;
     }
 
-    // simulate
-    setTimeout(runSimulation, 1000);
-
     setGrid(g => {
       return produce(g, gridCopy => {
         for (let i = 0; i < numRows; i++) {
           for (let k = 0; k < numCols; k++) {
             let neighbors = 0;
 
-            if (gridCopy[i][k + 1] === 1) {
-              neighbors += 1;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
             }
           }
         }
       });
     });
 
-    // const newGrid = produce();
+    setTimeout(runSimulation, 1000);
   }, []);
 
   return (
     <>
       <div>
-        <button onClick={() => setRunning(!running)}>
+        <button
+          onClick={() => {
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+              runSimulation();
+            }
+          }}
+        >
           {running ? "Stop" : "Start"}
         </button>
       </div>
